30ac003fefe990acb13ae9fe62202eceb56bbe71
ihrthk <ihrthk@gmail.com>
2014-12-06 15:45:19 +0800

    add parse res xml ,can to config

--- src/test/groovy/pl/droidsonroids/gradle/localization/LocalizationPluginTest.groovy
+++ src/test/groovy/pl/droidsonroids/gradle/localization/LocalizationPluginTest.groovy
@@ -1,6 +1,5 @@
 package pl.droidsonroids.gradle.localization
 
-import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.Sheet
 import org.apache.poi.ss.usermodel.Workbook
@@ -73,19 +72,42 @@ class LocalizationPluginTest extends GroovyTestCase {
     @Test
     void testWriteXls() {
 
-        def map = get(new File(getClass().getResource('res').getPath()))
-        writer(new File("/Users/zhangls/AndroidStudioProjects/ccc.xlsx"), map)
+//        def map = getMap(new File(getClass().getResource('res').getPath()))
+
+//        writer(file, map)
+        def lanuages = ['', 'cs', 'de', 'es', 'fr', 'hu', 'it', 'ja', 'ko', 'nl', 'pl', 'pt-rBR',
+                        'ru', 'sv', 'zh-rCN', 'zh-rTW'] as String[]
+        def sourceDir = new File("/Users/zhangls/AndroidStudioProjects/android/lite/res")
+        def files = filter(sourceDir, lanuages)
+        def map = getMap(files);
+
+        def outFile = new File("/Users/zhangls/AndroidStudioProjects/ccc.xlsx")
+        writer(outFile, map)
 
     }
 
-    private static Map<String, HashMap<String, String>> get(dir) {
+    private static File[] filter(dir, strs) {
+        File[] files = new File[strs.length]
+        int i = 0;
+        strs.each {
+            def s = ''.equals(it) ? "values" : "values-" + it
+            def var = dir.getPath() + "/" + s
+            File file = new File(var, "strings.xml")
+            if (file.exists()) {
+                files[i] = file
+            } else {
+                throw new IllegalArgumentException(file.getAbsolutePath() + " not exist")
+            }
+            i++
+        }
+        return files
+    }
+
+    private static Map<String, HashMap<String, String>> getMap(File[] files) {
         Map<String, HashMap<String, String>> map = new LinkedHashMap<String, HashMap<String, String>>()
-
-        dir.listFiles().each {
-
-            String subDir = it.getName()
-
-            new XmlParser().parse(new File(it, "strings.xml")).each {
+        files.each {
+            String subDir = it.getParentFile().getName()
+            new XmlParser().parse(it).each {
                 it.toString()
                 def name = it.attributes().get('name')
                 def value = it.value().text();
@@ -107,15 +129,13 @@ class LocalizationPluginTest extends GroovyTestCase {
         //创建sheet对象
         Sheet sheet1 = (Sheet) wb.createSheet("sheet1");
         //循环写入行数据
-
         int i = 0
-        for (String key : map.keySet()) {
+        map.each {
             Row row = (Row) sheet1.createRow(i);
-            row.createCell(0).setCellValue(key)
+            row.createCell(0).setCellValue(it.key)
             int j = 1
-            def m = map.get(key)
-            for (String value : m.keySet()) {
-                row.createCell(j).setCellValue(m.get(value))
+            it.value.each {
+                row.createCell(j).setCellValue(it.value)
                 j++
             }
             i++
