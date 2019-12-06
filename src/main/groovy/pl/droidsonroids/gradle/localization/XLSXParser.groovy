6c08d26e400a9afcf69b3cdd54a5adda972ccf81
nonda <nonda95@gmail.com>
2019-12-06 12:23:42 +0100

    Add option to evaluate formulas in XLSX files

--- src/main/groovy/pl/droidsonroids/gradle/localization/XLSXParser.groovy
+++ src/main/groovy/pl/droidsonroids/gradle/localization/XLSXParser.groovy
@@ -1,34 +1,32 @@
 package pl.droidsonroids.gradle.localization
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
-import org.apache.poi.ss.usermodel.Row
-import org.apache.poi.ss.usermodel.Sheet
-import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.ss.usermodel.*
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 class XLSXParser implements Parser {
 
     private Map<String, String[][]> mAllSheets
 
-    XLSXParser(InputStream inputStream, boolean isXls, String sheetName, boolean useAllSheets) {
+    XLSXParser(InputStream inputStream, boolean isXls, String sheetName, boolean useAllSheets, boolean evaluateFormulas) {
         Workbook workbook = isXls ? new HSSFWorkbook(inputStream) : new XSSFWorkbook(inputStream)
         workbook.setMissingCellPolicy(Row.MissingCellPolicy.CREATE_NULL_AS_BLANK)
-
+        FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator()
         if (useAllSheets) {
             mAllSheets = new HashMap<>(workbook.numberOfSheets)
             for (Sheet sheet : workbook.sheetIterator()) {
-                mAllSheets.put(sheet.getSheetName(), getAllValues(sheet))
+                mAllSheets.put(sheet.getSheetName(), getAllValues(sheet, evaluator, evaluateFormulas))
             }
         } else {
             mAllSheets = new HashMap<>(1)
             Sheet sheet = sheetName ? workbook.getSheet(sheetName) : workbook.getSheetAt(0)
             if (sheet == null)
                 throw new IllegalArgumentException("Sheet $sheetName does not exist")
-            mAllSheets.put(null, getAllValues(sheet))
+            mAllSheets.put(null, getAllValues(sheet, evaluator, evaluateFormulas))
         }
     }
 
-    static String[][] getAllValues(Sheet sheet) {
+    static String[][] getAllValues(Sheet sheet, FormulaEvaluator evaluator, boolean evaluateFormulas) {
         String[][] allCells = new String[sheet.lastRowNum + 1][]
         for (int i = 0; i <= sheet.lastRowNum; i++) {
             Row row = sheet.getRow(i)
@@ -36,7 +34,9 @@ class XLSXParser implements Parser {
                 continue
             allCells[i] = new String[row.lastCellNum]
             for (int j = 0; j < row.lastCellNum; j++) {
-                allCells[i][j] = row.getCell(j).toString() ?: ""
+                Cell cell = row.getCell(j)
+                if (evaluateFormulas) evaluator.evaluateInCell(cell)
+                allCells[i][j] = cell.toString() ?: ""
             }
         }
         return allCells
--- src/test/groovy/pl/droidsonroids/gradle/localization/ParseXLSXTest.groovy
+++ src/test/groovy/pl/droidsonroids/gradle/localization/ParseXLSXTest.groovy
@@ -82,4 +82,37 @@ class ParseXLSXTest extends LocalizationPluginTestBase {
         config.xlsFileURI = 'https://docs.google.com/a/droidsonroids.pl/spreadsheets/d/1sfE3Zk_7syHpq3HPKYQ9gRidm1W7c1IjIfdH1R8z9m4/export?format=xlsx'
         parseTestFile(config)
     }
+
+    @Test
+    void testXlsxFileWithFormulas() {
+        def name = 'valid_formulas.xlsx'
+
+        ConfigExtension config = new ConfigExtension()
+        config.xlsFileURI = getClass().getResource(name).toString()
+        config.allowEmptyTranslations = true
+        config.skipInvalidName = true
+        config.skipDuplicatedName = true
+        config.evaluateFormulas = true
+        config.defaultColumnName = 'EN'
+        config.nameColumnName = 'Android'
+        config.ignorableColumns.add('WinPhone')
+        config.ignorableColumns.add('iOS')
+        config.ignorableColumns.add('END')
+        config.defaultLocaleQualifier = 'en'
+        config.useAllSheets = true
+
+        def resDir = parseTestFile(config)
+
+        resDir.traverse(type: FileType.FILES) {
+            def filePath = it.path.replace(resDir.path, '')
+            def expectedFileURL = getClass().getResource("parsed_valid_formulas_xlsx/$filePath")
+            def diff = DiffBuilder.compare(Input.fromFile(it))
+                    .withTest(Input.fromURL(expectedFileURL))
+                    .ignoreWhitespace()
+                    .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byNameAndAllAttributes))
+                    .checkForSimilar()
+                    .build()
+            assertThat(diff.hasDifferences()).as('file: %s is different than expected %s', filePath, diff.toString()).isFalse()
+        }
+    }
 }
